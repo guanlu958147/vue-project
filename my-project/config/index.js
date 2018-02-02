ce47abff51a663c63964499dc34463a537142b0e
530990200 <530990200@qq.com>
2018-02-02 20:46:47 +0800

    影院静态页面，分页

--- my-project/config/index.js
+++ my-project/config/index.js
@@ -10,7 +10,15 @@ module.exports = {
     // Paths
     assetsSubDirectory: 'static',
     assetsPublicPath: '/',
-    proxyTable: {},
+    proxyTable: {   //在这里配置代理
+      "/api":{
+        target:"http://127.0.0.1:3000",
+        changeOrigin:true,
+        pathRewrite:{
+          "^/api":""
+        }
+      }
+    },
 
     // Various Dev Server settings
     host: 'localhost', // can be overwritten by process.env.HOST
--- my-project/src/components/info/info.vue
+++ my-project/src/components/info/info.vue
@@ -39,7 +39,7 @@
                                 <i class="el-icon-menu"></i>
                                 <span slot="title">新增影院</span>
                             </el-menu-item>
-                            <el-menu-item index="3-2" route="/info/studio">
+                            <el-menu-item index="3-2" route="/info/studioList">
                                 <i class="el-icon-menu"></i>
                                 <span slot="title">影院列表</span>
                             </el-menu-item>
@@ -54,12 +54,12 @@
                             <span slot="title">放映厅列表</span>
                         </el-menu-item>
                     </el-submenu>
-                    <el-submenu index="4">
+                    <el-submenu index="5">
                         <template slot="title">
                             <i class="el-icon-location"></i>
                             <span>排片管理</span>
                         </template>
-                        <el-menu-item index="4-1" route="/info/schedule">
+                        <el-menu-item index="5-1" route="/info/schedule">
                             <i class="el-icon-menu"></i>
                             <span slot="title">排片列表</span>
                         </el-menu-item>
