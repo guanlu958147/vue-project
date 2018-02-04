65e9b175c20f5c4b29e8838dbf390194cdb2ef11
blue <1060954183@qq.com>
2018-02-04 22:22:35 +0800

    no message

--- my-project/config/index.js
+++ my-project/config/index.js
@@ -6,47 +6,28 @@ const path = require("path");
 
 module.exports = {
   dev: {
-<<<<<<< HEAD
+    //Paths
+    assetsSubDirectory: "static",
+    assetsPublicPath: "/",
+    proxyTable: {
+      "/studios/*": {
+        target: "http://127.0.0.1:3000",
+        //是否跨域
+        changeOrigin: true
+      }
+    },
     // Paths
     assetsSubDirectory: "static",
     assetsPublicPath: "/",
     proxyTable: {
       "/api": {
-        target: "http://localhost:3000", //设置你调用的接口域名和端口号 别忘了加http
+        target: "http://127.0.0.1:3000",
         changeOrigin: true,
         pathRewrite: {
-          "^/api": "" //这里理解成用‘/api’代替target里面的地址，后面组件中我们掉接口时直接用api代替 比如我要调用'http://40.00.100.100:3002/user/add'，直接写‘/api/user/add’即可
+          "^/api": ""
         }
       }
     },
-=======
-    //Paths
-    assetsSubDirectory:'static',
-    assetsPublicPath:'/',
-    proxyTable:{
-      '/studios/*':{
-        target:"http://127.0.0.1:3000",
-        //是否跨域
-        changeOrigin:true
-       
-      }
-    },
-    // Paths
-<<<<<<< HEAD
-=======
-    assetsSubDirectory: 'static',
-    assetsPublicPath: '/',
-    proxyTable: {
-      '/api':{
-        target:"http://127.0.0.1:3000",
-        changeOrigin:true,
-        pathRewrite:{
-          '^/api':''
-        }
-      }
-    },
->>>>>>> f157bc5f0bbb954932665b989a8caf15a8413302
->>>>>>> 8b8b5ba41537225c0eece74fb9d9351b652216af
 
     // Various Dev Server settings
     host: "localhost", // can be overwritten by process.env.HOST
