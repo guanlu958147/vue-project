6b52567d2ab915b5df0e4f2135804db8b207d67e
xiangYoung <xiangyoung@foxmail.com>
2018-02-05 11:19:13 +0800

    完善user.vue,userStore.js。用户列表的查询，修改，删除

--- my-project/src/components/user/userStore.js
+++ my-project/src/components/user/userStore.js
@@ -1,12 +1,41 @@
 const userStore = {
-    state:{
-
+    namespaced: true,
+    state: {
+        curPage: 1,
+        eachPage: 10,
+        total: 0,
+        rows: []
     },
-    mutations:{
-
+    mutations: {
+        getUsersByPage(state, payload) {
+            Object.assign(state, payload)
+        },
+        setCurPage(state, { curPage }) {
+            console.log(curPage)
+            state.curPage = curPage
+        },
+        setEachPage(state, { eachPage }) {
+            state.eachPage = eachPage
+        }
     },
-    actions:{
-
+    actions: {
+        async getUsersByPageAsync(context) {
+            const { curPage, eachPage } = context.state
+            const data = await fetch(
+              "/api/users/getUserList?curPage=" + curPage + "&eachPage=" + eachPage,
+              {
+                method: "GET",
+                headers: {
+                  "Content-Type": "application/x-www-form-urlencoded"
+                },
+                // body: "username=" + username + "&password=" + password,
+                credentials: "include"
+              }
+            ).then(function (response) {    
+              return response.json();
+            });
+            context.commit("getUsersByPage", data)
+          }
     }
 }
 export default userStore;
