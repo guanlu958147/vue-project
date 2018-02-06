54ce71a0da634baaa7a4b02ce80d408f3e22fe83
quanwei <543129715@qq.com>
2018-02-06 11:46:44 +0800

    排片列表

--- my-project/config/index.js
+++ my-project/config/index.js
@@ -6,20 +6,8 @@ const path = require('path')
 
 module.exports = {
   dev: {
-    //Paths
-    assetsSubDirectory:'static',
-    assetsPublicPath:'/',
-    proxyTable:{
-      '/studios/*':{
-        target:"http://127.0.0.1:3000",
-        //是否跨域
-        changeOrigin:true
-       
-      }
-    },
+   
     // Paths
-<<<<<<< HEAD
-=======
     assetsSubDirectory: 'static',
     assetsPublicPath: '/',
     proxyTable: {
@@ -31,7 +19,6 @@ module.exports = {
         }
       }
     },
->>>>>>> f157bc5f0bbb954932665b989a8caf15a8413302
 
     // Various Dev Server settings
     host: 'localhost', // can be overwritten by process.env.HOST
